c08d5ec5958a6f50e1b465baca776b9d1ffc58b5
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-12-09 20:42:48 -0300

    First commit

---
+++ MyPace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPace
+//
+//  Created by Carlos Felipe Araújo on 09/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPace/MyPaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPaceApp.swift
+//  MyPace
+//
+//  Created by Carlos Felipe Araújo on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
